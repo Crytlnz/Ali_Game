10eeeb97b2b756f2e2dd038f1b1d349de91c3c3f
Crytlnz <muhsyafali27@gmail.com>
2025-03-10 11:16:56 +0700

    Menghapus BigManJD

--- 2D/Assets/Script/Knight.cs
+++ 2D/Assets/Script/Knight.cs
@@ -6,21 +6,28 @@ public class Knight : MonoBehaviour
 {
     public float walkSpeed = 3f;
 
-    
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(walkSpeed * Vector2.right.x, rb.velocity.y);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-       
+   
     }
 
     // Update is called once per frame
     void Update()
     {
-        walkSpeed = Time.deltaTime;
     
     }
-    
-    
 }
  
